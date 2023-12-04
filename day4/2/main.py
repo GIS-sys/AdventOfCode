90e98d50a399844f7ecb9ddd950d1b5f1594bee5
EgorovGA <egorov.ga@minc.tech>
2023-12-04 08:15:50 +0300

    day4 done

--- day4/2/main.py
+++ day4/2/main.py
@@ -1,7 +1,9 @@
 with open("output.txt", "w") as fout:
     with open("input.txt", "r") as fin:
+        copies = {0: 1}
         s = 0
-        for line in fin:
+        for i, line in enumerate(fin):
+            #print(i, copies, s)
             if line.strip() == "":
                 continue
             line = line[line.index(":")+2:]
@@ -13,9 +15,9 @@ with open("output.txt", "w") as fout:
             cur = 0
             for x in winning:
                 if x in my:
-                    if cur == 0:
-                        cur = 1
-                    else:
-                        cur *= 2
-            s += cur
+                    cur += 1
+            for j in range(1, cur+1):
+                copies[i + j] = copies.get(i + j, 1) + copies.get(i, 1)
+            s += cur * copies.get(i, 1)
+        s += i + 1
         fout.write(str(s))
