b5f32b4c7165f3f175f8e5f2011cc18e8a58467c
EgorovGA <egorov.ga@minc.tech>
2023-12-13 15:53:03 +0300

    day12 task1 - debugging, add comments

--- day12/1/main.py
+++ day12/1/main.py
@@ -3,29 +3,40 @@ import numpy as np
 
 
 def permutations(record, numbers, history=""):
-    while len(numbers) != 0 and numbers[0] == 0:
-        numbers = numbers[1:]
-        if len(record) != 0:
-            if record[0] == "#":
-                return 0
-            record = record[1:]
-            history = history + "."
+    # remove all leading "."
+    #   if we were on a streak - return 0, cant break continuity
     while len(record) != 0 and record[0] == ".":
+        if numbers[0] != 0:
+            return 0
         record = record[1:]
         history = history + "."
-    print(f"perm {record=}, {numbers=}")
+    # debug
+    print(f"perm {record=}, {numbers=}, {history=}")
+    # exit conditions:
+    #   if no more records, exit, checking if no more numbers
+    #   if no more numbers, exit, checking if all other records are not mandatory
     if len(record) == 0:
-        if len(numbers) == 0:
+        if len(numbers) == 0 or numbers == [0]:
             print(history)
-        return 0 if len(numbers) != 0 else 1
-    if len(numbers) == 0:
+        return 1 if len(numbers) == 0 or numbers == [0] else 0
+    if len(numbers) == 0 or numbers == [0]:
         if not ("#" in record):
             print(history + "." * len(record))
-        return 0 if ("#" in record) else 1
-    if record[0] == "#":
+        return 1 if not ("#" in record) else 0
+    # recursion
+    #   if not on a streak
+    #     if # then we begin streak
+    #     if ? then either skip or begin streak
+    #   if on a streak, next one is mandatory #
+    if numbers[0] == 0:
+        if record[0] == "#":
+            return permutations(record[1:], [numbers[1]-1] + numbers[2:], history+"#")
+        elif record[0] == "?":
+            return permutations(record[1:], [numbers[1]-1] + numbers[2:], history+"#") + permutations(record[1:], numbers, history+".")
+        else:
+            raise Exception("wtf")
+    else:
         return permutations(record[1:], [numbers[0]-1] + numbers[1:], history+"#")
-    if record[0] == "?":
-        return permutations(record[1:], [numbers[0]-1] + numbers[1:], history+"#") + permutations(record[1:], numbers, history+".")
     raise Exception("wtf")
 
 with open("output.txt", "w") as fout:
@@ -37,7 +48,7 @@ with open("output.txt", "w") as fout:
                 continue
             record, numbers = line.split(" ")
             numbers = list(map(int, numbers.split(",")))
-            new_perm = permutations(record, numbers)
+            new_perm = permutations(record, [0] + numbers)
             print(f"{record=}, {numbers=}, {new_perm=}" + "\n\n\n")
             s += new_perm
         fout.write(str(s))
